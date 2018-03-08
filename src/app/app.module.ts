26b662790db94793b308fb1d410be16e7aa0057b
BogdanJAVA <bogdanshishkin1998@gmail.com>
2018-03-08 23:04:08 +0300

    rest, checkout order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { StoreModule } from './store/store.module';
+import { StoreComponent } from './store/store.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { StoreFirstGuard } from './storeFirst.guard';
 
 
 @NgModule({
@@ -11,9 +15,24 @@ import { StoreModule } from './store/store.module';
     AppComponent
   ],
   imports: [
-    BrowserModule, StoreModule
+    BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      {
+        path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path: "**", redirectTo: "/store" }
+    ])
   ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/cart.model.ts
@@ -0,0 +1,64 @@
+import { Product } from "./product.model";
+import { Injectable } from "@angular/core";
+
+/**
+ * @description A class that provides functionality for 
+ *  working with the consumer cart
+ */
+ 
+@Injectable()
+export class Cart {
+    public lines: CartLine[] = [];
+    public itemCount: number = 0;
+    public cartPrice: number = 0;
+
+    addLine(product: Product, quantity: number = 1) {
+        let line = this.lines.find(line => line.product.id == product.id);
+        if (line != undefined) {
+            line.quantity += quantity;
+        } else {
+            this.lines.push(new CartLine(product, quantity));
+        }
+        this.recalculate();
+    }
+
+    updateQuantity(product: Product, quantity: number) {
+        let line = this.lines.find(line => line.product.id == product.id);
+        if (line != undefined) {
+            line.quantity = Number(quantity);
+        }
+        this.recalculate();
+    }
+
+    removeLine(id: number) {
+        let index = this.lines.findIndex(line => line.product.id == id);
+        this.lines.splice(index);
+        this.recalculate();
+    }
+
+    clear() {
+        this.lines = [];
+        this.itemCount = 0;
+        this.cartPrice = 0;
+    }
+
+    private recalculate() {
+        this.itemCount = 0;
+        this.cartPrice = 0;
+        this.lines.forEach(line => {
+            this.itemCount += line.quantity;
+            this.cartPrice += (line.quantity * line.product.price);
+        })
+    }
+
+}
+
+export class CartLine {
+
+    constructor(public product: Product,
+        public quantity: number) { }
+
+    get lineTotal() {
+        return this.quantity * this.product.price;
+    }
+}
